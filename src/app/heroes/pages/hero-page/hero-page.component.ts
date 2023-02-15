65d3c743c97209b671e5c9582774e629c0894d28
Adrian <adrian.martinsz201@gmail.com>
2023-02-15 08:35:23 -0500

    [ADD]: get hero by id

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-hero-page',
   templateUrl: './hero-page.component.html',
-  styles: [
-  ]
 })
-export class HeroPageComponent {
+export class HeroPageComponent implements OnInit {
+  public hero?: Hero; // en el init es undefined, hasta q se haga la peticion y traiga el hero
 
+  constructor(
+    private readonly heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute, // obtener el param de la URL
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id))) //smap modifica data
+      .subscribe((hero) => {
+        // si no viene 1 hero, lo saco de la ruta
+        if (!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        console.log(hero);
+        return;
+      });
+  }
+
+  goBack(): void {
+    this.router.navigateByUrl('/heroes/list');
+  }
 }
